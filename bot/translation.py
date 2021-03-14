c901005b1ee67b76e77863b746ae13496a08fcbc
naseef12356 <80581006+naseef12356@users.noreply.github.com>
2021-03-14 17:53:20 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -1,40 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) @AlbertEinsteinTG
+# (c) @CFCHART
 
 class Translation(object):
     
     START_TEXT = """<b>Hai {}!!</b>
-<i>Am Just A Simple Hand Auto Filter Bot_ Bot For Searching Files From Channel...
-
-Just Sent Any Text I Will Search In All Connected Chat And Reply You With The Message link
-
-You Can Even Connected To 3 Channels At A Time...</i>"""    
+<i>I AM PROPERTY OF @CFCHART AND @CFCHATOFFICIAL</i>"""    
     
     HELP_TEXT = """<i><u>Usage Guide</u></i>
 
-<i>=> Add Bot To Any Channel As Admin With Add Members/ Invite Users Via Link
+<i>OLD MOVIES 
+https://t.me/joinchat/7hP8iQnONVdlY2M9
 
-=> Copy Channel ID
+TELUGU MOVIES 
+https://t.me/joinchat/U4Gw_XLpQ_SLO6i9
 
-=> Use <code>/connect {channel id}</code> In Your Group To Connect With The Group
+ALL MOVIES
+https://t.me/joinchat/R2NzDOyi85wb4EEp
 
-=> Use <code>/disconnect {channel id}</code> In Your Group To Disconnect From Your Group
+MALAYALAM MOVIES
+https://t.me/joinchat/QSPu1QHGNIQ2MmQ9
 
-=> Use <code>/delall</code> In Your Group To Clear All Your Group  Connections (Owner Only)
+SERIES AND FILMOGRAPHY 
+https://t.me/joinchat/R3ocwmbOOA3ASKYJ
 
-Now You Are All Set And Ready To Go...
+DVD UPDATES 
+https://t.me/joinchat/VUMDWO9PFmjruguU
 
-Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
+MALAYALAM SUBTITLES 
+https://t.me/joinchat/VEPyuGL5nV4ZV10G
+
+COPYRIGHTED MOVIES
+https://t.me/joinchat/UfGEgtRQzZrXUi8Z
+From these CHANNEL To Provide You To The Link</i>
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code>DULQUER 3.0</code>
 
-<b>➥ Creator</b> : <b><i><a herf="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a herf="https://t.me/Cfchatofficial">Cinema Factory</a></i></b>
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>➥ Language</b> : <code>English</code>
 
-<b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
+<b>➥ Library</b> : <i><a herf="https://t.me/CFCHART">സിനിമ ഫാക്ടറി</a></i>
 
-<b>➥ Source Code</b> : <i><a herf="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">GitHub</a></i>
 """
